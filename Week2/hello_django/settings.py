9c073a6cdb654ededf9a6bf11577628f55cedecc
B1344048 <89004211+smallfire1122@users.noreply.github.com>
2026-03-11 14:22:55 +0800

    Update settings.py

--- Week2/hello_django/settings.py
+++ Week2/hello_django/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "myhello",
+    'myhello',
 ]
 
 MIDDLEWARE = [
@@ -134,4 +134,5 @@ LOGGING = {
             'propagate': True,
         },
     },
+
 }
